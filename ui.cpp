2d76b27b18c3fb3fb0eddfe9b3098aa2174084db
Unknown <levipoulton439@gmail.com>
2025-05-25 17:38:55 +1200

    Add display functions for Player class

--- ui.cpp
+++ ui.cpp
@@ -1,7 +1,8 @@
 // PVS-Studio Static Code Analyzer for C, C++, C#, and Java: https://pvs-studio.com
-
 #include <iostream>
-
+#include <memory>
+#include <array>
+#include "players.h"
 using namespace std;
 
 void clear() {
@@ -19,9 +20,15 @@ void wait() {
 }
 
 void displayOpts() {
-	cout << "Would you like to: cast a healing spell (costs 1 level), attack a enemy, or go exploring? \nEnter 'heal', 'attack', or 'explore' to start the action. To open inventory, enter 'inventory' (doesn't count as an action). \n";
+	cout << "Would you like to: cast a healing spell (costs 1 level), attack a enemy, or go exploring? \nEnter 'heal', 'attack', or 'explore' to start the action. To open inventory, enter 'inventory' (doesn't count as an action)." << endl;
 }
 
 void displayFleeOpts(float fleeCost) {
-	cout << "Would you like to, attack or flee (lose " << fleeCost << " levels)? To open inventory enter 'inventory' (doesn't count as an action). \n Enter 'attack' or 'flee'. \n";
+	cout << "Would you like to, attack or flee (lose " << fleeCost << " levels)? To open inventory enter 'inventory' (doesn't count as an action). \n Enter 'attack' or 'flee'." << endl;
+}
+
+void displayPlayers(array<unique_ptr<Players>, 2>& players) {
+	for (const auto& player : players) {
+		player->display();
+	}
 }
--- ui.h
+++ ui.h
@@ -1,7 +1,11 @@
 #pragma once
+#include <memory>
+#include <array>
+#include "players.h"
 
 void clear();
 void wait();
 
 void displayOpts();
 void displayFleeOpts(float fleeCost);
+void displayPlayers(std::array<std::unique_ptr<Players>, 2>& players);
