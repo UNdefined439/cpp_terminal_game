2194af5944e32f6f6a1ab197c26d5a833f7a28bf
Unknown <levipoulton439@gmail.com>
2025-05-25 17:35:09 +1200

    Add logic for Dugeon event

--- events.cpp
+++ events.cpp
@@ -1,1 +1,86 @@
 // PVS-Studio Static Code Analyzer for C, C++, C#, and Java: https://pvs-studio.com
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include "events.h"
+#include "ui.h"
+#include "utility.h"
+#include "players.h"
+#include "gameObjects.h"
+
+using namespace std;
+
+void explore(Players& human) {
+	int num = rng(1, 100);
+	if (num < 50)
+		dungeon(human);
+	else if (num < 75)
+		merchant(human);
+	else if (num < 90)
+		treasure(human);
+	else
+		dragon(human);
+}
+
+void dungeon(Players& human) {
+	clear();
+	string name = human.getName();
+	cout << name << ":" << endl << endl;
+	loadingDots(1, 500, "You pick up a torch and cowering behind your shield enter the darkness");
+	this_thread::sleep_for(chrono::milliseconds(1000));
+	clear();
+		
+	Players monster = []() {
+		int num = rng(1, 100);
+		if (num < 35) return Players(0, 0, 0, "TREASURE");
+		if (num < 55) return Players(0, 0, 0, "Zombie");
+		if (num < 75) return Players(0, 0, 0, "Booby Traps");
+		if (num < 90) return Players(0, 0, 0, "Skeleton");
+		return Players(0, 0, 0, "Ghost");
+	}();
+	
+	if (monster.getName() == "TREASURE") {
+
+	} else {
+		string msg = name + " has come across a room of " + monster.getName();
+		loadingDots(1, 500, msg);
+		this_thread::sleep_for(chrono::milliseconds(1000));
+
+		int turns = 1;
+		bool flee = false;
+		while (monster.isAlive() && human.isAlive()) {
+			clear();
+			human.display();
+			monster.display();
+
+			if (turns % 2 == 1) {
+				// monster
+			} else {
+				string action;
+				do {
+					break;
+				} while (action != "attack" && action != "flee");
+
+				if (action == "flee") {
+					flee = true;
+					break;
+				} else {
+
+				}
+			}
+			++turns;
+		}
+	}
+}
+
+void merchant(Players& human) {
+
+}
+
+void treasure(Players& human) {
+
+}
+
+void dragon(Players& human) {
+
+}
--- events.h
+++ events.h
@@ -1,1 +1,8 @@
 #pragma once
+#include "players.h"
+
+void explore(Players& human);
+void dungeon(Players& human);
+void merchant(Players& human);
+void treasure(Players& human);
+void dragon(Players& human);
